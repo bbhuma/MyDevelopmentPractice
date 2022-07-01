5dc1a4bb05c6107e9820c25a24887b06f0005cfb
bala gangadhar reddy bhuma <bgrbhuma@gmail.com>
2022-07-01 18:30:42 -0400

    Learn JS

---
+++ JavaSciptPractice_Jason/starter/script.js
@@ -0,0 +1,39 @@
+/*
+VALUES and VARIABLES
+let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN!");
+// js = "boring";
+// if (js === "amazing") alert("JavaScript is FUN!");
+// else alert("JS is boring now!");
+100 + 90 + 203;
+console.log(100 + 90 + 203); //Passing literals
+
+let firstname = "Bala"; //if you change this to some othe rname you get that value.
+console.log(firstname); //passing variables instead of literals, JS replaces the firstname with the value.
+
+if i need to change the value, you change at only one place you declared.
+use camel casing for variables.
+
+Rules of naming variables - 
+1. variable name can not start with a number, invalid or unexpected token occured.
+
+let myFirstJob = "Programmer"; //self descriptive varible names
+let myCurrentJob = "Teacher";
+
+let myCountry = "India";
+let myContinent = "Asia";
+let populationinMillions = "10Millions";
+let PI = 3.1415;
+
+console.log(myCountry);
+console.log(myContinent);
+console.log(populationinMillions);
+*/
+/*
+DATA TYPES - 
+true; // value of type BOOLEAN, printed in voilet
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true); // Print boolean
+*/
