20f160afaebe436edcb712114cd1612dabba2a18
bala gangadhar reddy bhuma <bgrbhuma@gmail.com>
2022-07-03 13:16:31 -0400

    coding challenge#2 JS  (#12)
    * Practice HTML CSS
    
    * Coding challenge#2
    
    * update

---
+++ JavaSciptPractice_Jason/starter/codingChallenge2.js
@@ -0,0 +1,7 @@
+if (isMarkHigherBMI)
+  console.log(
+    // "Mark's BMI (" + BMIMark + ") is higher than John's (" + BMIJohn + ")!"
+    `Mark's BMI (${BMIMark}) is higher than John's ( ${BMIJohn} )❤️!`
+  );
+else
+  console.log(`Mark's BMI (${BMIMark}) is lesser than John's ( ${BMIJohn} )!`);
